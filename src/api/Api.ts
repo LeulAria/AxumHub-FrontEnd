f407a7ecff0b92afd7fb5d1ac68f3c5a0ce70c35
LeulAria <leulsegedtariku@gmail.com>
2020-07-27 04:24:35 -1200

    prod base url

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 // deveopment env base url
-export const baseURL = "http://localhost:8000/api"
+// export const baseURL = "http://localhost:8000/api"
 
 // production env base url
-// export const baseURL = "https://axumhub.herokuapp.com/api"
+export const baseURL = "https://axumhub.herokuapp.com/api"
 
 const Api = axios.create({
   baseURL,
